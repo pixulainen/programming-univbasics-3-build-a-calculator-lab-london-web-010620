64e8c8c570dc776cdd82dc49261104fab4a1cf79
George Apetrei <george.apetrei@gmail.com>
2019-12-15 18:53:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,11 +3,13 @@ num2 +num1
 end
 
 def subtraction(num1, num2)
-num2 - num1
+result = num2 - num1
+return result
 end
 
 def division(num1, num2)
-num2 / num1
+result = num2 / num1
+return result
 end
 
 def multiplication(num1, num2)
@@ -15,7 +17,8 @@ num1 * num2
 end
 
 def modulo(num1, num2)
-num2 % num1
+result=num2 % num1
+return result
 end
 
 def square_root(num)
